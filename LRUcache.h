a88f6b27d91cedb70e58366a6f10805ff233a1b1
colepw <colepw7@gmail.com>
2025-12-16 00:13:59 -0800

    LRUCache.h

---
+++ LRUcache.h
@@ -0,0 +1,43 @@
+#ifndef LRUCACHE_H
+#define LRUCACHE_H
+
+#include <unordered_map>
+#include <memory>
+#include <cstddef>
+#include <optional>
+
+class LRUCache {
+    public:
+        explicit LRUCache(std::size_t capacity);
+        LRUCache(const LRUCache&) = delete;
+        LRUCache& operator=(const LRUCache&) = delete;
+        ~LRUCache() = default;
+
+        std::optional<int> get(int key);
+        void put(int key, int value);
+        std::size_t size() const;
+        bool contains(int key) const;
+        bool erase(int key);
+        void clear();
+
+    private:
+        struct Node {
+            Node* next;
+            Node* prev;
+            int key;
+            int value;
+        };
+
+        Node head_;
+        Node tail_;
+        std::unordered_map<int, std::unique_ptr<Node>> store_;
+        std::size_t capacity_;
+
+        void detach(Node* n);
+        void attach_front(Node* n);
+        void move_to_front(Node* n);
+        void evict_lru();
+        void check_invariants() const;
+};
+
+#endif // LRUCACHE_H
